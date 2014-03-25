04791767b3c0709a9e7ceb19727a1a06f51bbdc2
egomoll80 <ericgomoll@gmail.com>
2014-03-25 16:24:26 -0500

    test alert

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     
     $('.buttonpusher').click(function(e){
+        alert("!!!");
         $('.hiddentext').stop().slideToggle(300);
         e.preventDefault();
     });
